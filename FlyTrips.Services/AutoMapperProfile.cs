cfe737f4d16295e5646776c3de00d23ca10b1f3f
Mihaela Hristova <106870022+Mihaela0278@users.noreply.github.com>
2022-12-17 13:35:49 +0200

    Create mapper

---
+++ FlyTrips.Services/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FlyTrips.DTO.Role;
+using FlyTrips.Entities;
+
+namespace FlyTrips.Services
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<RoleCreateUpdateDto, Role>();
+            CreateMap<Role, RoleResponseDto>();
+        }
+    }
+}
