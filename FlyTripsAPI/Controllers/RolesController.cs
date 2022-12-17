c404352e467a6c7f877d97666eec618057b2924a
Mihaela Hristova <106870022+Mihaela0278@users.noreply.github.com>
2022-12-17 13:36:49 +0200

    Create RolesController

---
+++ FlyTripsAPI/Controllers/RolesController.cs
@@ -0,0 +1,49 @@
+﻿using FlyTrips.DTO.Role;
+using FlyTrips.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlyTripsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly IRoleService roleService;
+
+        public RolesController(IRoleService roleService)
+        {
+            this.roleService = roleService;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] RoleCreateUpdateDto dto)
+        {
+            return Ok(roleService.Create(dto));
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(roleService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            return Ok(roleService.GetById(id));
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody] RoleCreateUpdateDto dto, [FromRoute] int id)
+        {
+            return Ok(roleService.Update(dto, id));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            roleService.Delete(id);
+            return NoContent();
+        }
+    }
+}
