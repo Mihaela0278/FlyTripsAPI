f1986670f384ab7aef3af5b131a1fdb9e7d3c563
Mihaela Hristova <106870022+Mihaela0278@users.noreply.github.com>
2022-12-17 13:36:12 +0200

    Create Base service and implementation

---
+++ FlyTrips.Services/BaseService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using FlyTrips.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlyTrips.Services
+{
+    public class BaseService<T> : IBaseService<T> where T : BaseEntity
+    {
+        protected readonly FlyTripsDbContext context;
+        protected readonly IMapper mapper;
+
+        public BaseService(FlyTripsDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public void Create(T entity)
+        {
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ FlyTrips.Services/IBaseService.cs
@@ -0,0 +1,17 @@
+﻿using FlyTrips.Entities;
+
+namespace FlyTrips.Services
+{
+    public interface IBaseService<T> where T : BaseEntity
+    {
+        T GetById(int id);
+
+        IEnumerable<T> GetAll();
+
+        void Create(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
