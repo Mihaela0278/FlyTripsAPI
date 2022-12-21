851f78c237e6d86f2fc5a7bde94f3227687ff567
Mihaela Hristova <106870022+Mihaela0278@users.noreply.github.com>
2022-12-21 14:40:49 +0200

    Add Airline DTOs

---
+++ FlyTrips.DTO/Airline/AirlineCreateUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlyTrips.DTO.Airline
+{
+    public class AirlineCreateUpdateDto
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Name must be at least 3 characters long!")]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Country must be at least 3 characters long!")]
+        public string Country { get; set; }
+
+    }
+}
